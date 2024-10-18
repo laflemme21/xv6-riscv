507ad51ef83039104ef1ec3f40f7aa9fb8fa95aa
laflemme21 <carlk3549@gmail.com>
2024-10-18 13:41:29 +0100

    hw

---
+++ user/hw.c
@@ -0,0 +1,7 @@
+#include "/uolstore/home/users/sc23ksk/OS/xv6-riscv/kernel/types.h"
+#include "/uolstore/home/users/sc23ksk/OS/xv6-riscv/user/user.h"
+
+int main(int argc, char *argv[]){
+    printf("Hello World");
+    exit(0);
+};
